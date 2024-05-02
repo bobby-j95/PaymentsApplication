d99bc3fdac192384212cbeb68aec35359f28d0d9
bobby-95 <robertjohns08@gmail.com>
2024-05-02 12:53:40 -0500

    added to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,50 @@ import Transactions from './components/Transaction';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PageHeader from './components/pageHeader/PageHeader';
 import NewTransaction from './components/NewTrans/NewTransaction';
+import { LoginContextType, UserType, LoginContext } from './context/context';
+import { useState } from 'react';
+import SignIn from './components/SignIn/SignIn';
 
 function App() {
+
+  const [user, setUser] = useState<UserType>({id:0, name:"", role:""});
+
+  console.log(user);
+
+  const login = (user: UserType) => {
+    setUser(user);
+  };
+
+  const logout = () => {
+    setUser({id:0, name:"", role:""});
+  };
+
+  const statefulContext: LoginContextType = {
+    user: user,
+    login: login,
+    logout: logout
+  };
+
   return (
-    <BrowserRouter>
-      <PageHeader />
-      <Routes>
-        <Route path="/find" element={
-          <>
-            {/* <label htmlFor="orderId">Order ID: </label>
-            <input type="string" id="order_Id"/>
-            <button onClick={SearchForID}>Search</button> */}
-            <Transactions />
-          </>
-        } />
-        <Route path="/add" element={<NewTransaction />} />
-        <Route path="/" element={<h1>Payments system</h1>} />
-        <Route path="*" element={<h1>Page Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <LoginContext.Provider value={statefulContext} >
+      <BrowserRouter>
+        <PageHeader />
+        <Routes>
+          <Route path="/signin" element={<SignIn />}> </Route>
+          <Route path="/find" element={
+            <>
+              {/* <label htmlFor="orderId">Order ID: </label>
+              <input type="string" id="order_Id"/>
+              <button onClick={SearchForID}>Search</button> */}
+              <Transactions />
+            </>
+          } />
+          <Route path="/add" element={<NewTransaction />} />
+          <Route path="/" element={<h1>Payments system</h1>} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </LoginContext.Provider>
   )
 }
 
--- src/components/pageHeader/Menu.tsx
+++ src/components/pageHeader/Menu.tsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { LoginContext, LoginContextType, UserType } from "../../context/context";
 
 
 const Menu = () : JSX.Element => {
+
+//if the user is loggin in, render their name
+//if not, then render sign in button
+    const contextPointer = useContext<LoginContextType>(LoginContext)
+
+    const userDetails : UserType = contextPointer.user;
     return (
         <ul className="nav">
             <li><Link to="/find">Find a transaction</Link></li>
             <li><Link to="/add">New transaction</Link></li>
+            <li>{userDetails.id === 0 ? (<Link to="/signin">Sign In</Link>) : (<p>Hello {userDetails.name}</p>)}</li>
         </ul>
     );
 }
